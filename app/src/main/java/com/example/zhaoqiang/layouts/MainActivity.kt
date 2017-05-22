45ef9e6a31ab6ba1def55bbd961e3549bdb325e4
zhaoqiang <727319870@qq.com>
2017-05-22 08:59:49 +0800

    xhz
    用代码实现添加布局

---
+++ app/src/main/java/com/example/zhaoqiang/layouts/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.zhaoqiang.layouts;
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+
+open class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+
+        val parent = v?.parent as LinearLayout
+        parent.removeView(v)
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val view = LayoutInflater.from(this).inflate(R.layout.activity_main, null)
+//        setContentView(view)
+
+        buildUI()
+
+    }
+
+    fun buildUI() {
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
+        setContentView(layout)
+
+        for (i in 0..10) {
+            val btn = Button(this)
+            btn.text = "Remove me"+i
+            btn.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            layout.addView(btn)
+            btn.setOnClickListener(this)
+
+
+        }
+    }
+}
